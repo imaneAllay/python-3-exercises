014f0ea9c938655d74290b6472ae452b835fb3c9
Imane Allay <iallay@xpanxion.com>
2023-07-05 16:55:59 -0400

    Add Ex1

--- src/ex1.py
+++ src/ex1.py
@@ -1,14 +1,16 @@
-from ValidationException import ValidationException
-
-
-def validate_file():
-    pass # TODO
-
-
+def validate_file(input_file):
+    with open(input_file) as mile_file:
+        next(mile_file)
+        for line_num, line in enumerate(mile_file, start=2):
+            car_id, mileage = line.split(',')
+            try:
+                mileage = int(mileage)
+            except ValueError:
+                print("Invalid mileage: " + str(mileage))
 
 def ex1():
     try:
-        validate_file("input.txt")
+        validate_file("../files/input.txt")
     except ValidationException as ve:
         print(ve)
 

88478f2082706be05c6815fcc2b2ce58014389a7
Karina Velasquez <kvelasquez@xpanxion.com>
2023-07-07 10:55:10 -0400

    Ex2

--- src/ex2.py
+++ src/ex2.py
@@ -1,10 +1,19 @@
+import csv
 
-def find_total_visits():
-    pass # TODO
-
+def find_total_ones():
+    total_ones = 0
+    file_paths = ["../files/week-1.csv", "../files/week-2.csv", "../files/week-3.csv"]
+    for file in file_paths:
+        with open(file, "r") as csv_file:
+            rows = csv.reader(csv_file, delimiter=',')
+            next(rows)  # Skip the header row
+            for row in rows:
+                for value in row[1:]:
+                    total_ones += int(value)
+    return total_ones
 
 def ex2():
-    total = find_total_visits()
-    print(f"Total visits: {total}.")
+    total_ones = find_total_ones()
+    print(f"Total ones: {total_ones}.")
 
 ex2()
